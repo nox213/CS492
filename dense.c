20abae6c140583a40d9823e8ed27e8851a1bb3d0
yoonjae <yoonjae@kaist.ac.kr>
2019-04-01 16:38:06 +0900

    Modify mul

--- dense.c
+++ dense.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <math.h>
+#include <float.h>
+#include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
 
 #define NANO_TO_SEC(x) ((long) ((x) / 10e9))
+#define NANO_TO_MILI(x) ((long) ((x) / 10e6))
 #define SEC_TO_NANO(x) ((long) ((x) * 10e9))
+#define SEC_TO_MILI(x) ((long) ((x) * 10e3))
 
 struct task {
 	int i;
@@ -29,6 +34,13 @@ pthread_mutex_t c_lock = PTHREAD_MUTEX_INITIALIZER;
 void print_array(int n, double arr[][n]);
 void init_task_queue(int n);
 void *mul_matrix(void *arg);
+void mul_matrix_single(int n, double (*a)[n], double (*b)[n], double (*answer)[n]);
+bool nearly_equal(double a, double b, double epsilon); 
+
+static inline min(const double a, const double b)
+{
+	return a < b ? a : b;
+}
 
 int main(int argc, char *argv[])
 {
@@ -81,9 +93,9 @@ int main(int argc, char *argv[])
 	}
 
 	init_task_queue(n);
-	aux.a = a;
-	aux.b = b;
-	aux.c = c;
+	aux.a = (double **) a;
+	aux.b = (double **) b;
+	aux.c = (double **) c;
 	aux.n = n;
 
 	clock_gettime(CLOCK_REALTIME, &begin);
@@ -94,8 +106,24 @@ int main(int argc, char *argv[])
 		pthread_join(p_threads[i], NULL);
 	clock_gettime(CLOCK_REALTIME, &end);
 	
-	elapsed = (end.tv_sec - begin.tv_sec) + (NANO_TO_SEC(end.tv_nsec - begin.tv_nsec));
-	printf("elapsed time: %ld (sec)\n", (elapsed));
+	elapsed = SEC_TO_NANO(end.tv_sec - begin.tv_sec) + ((end.tv_nsec - begin.tv_nsec));
+	printf("elapsed time: %ld (msec)\n", NANO_TO_MILI(elapsed));
+
+	/*
+	mul_matrix_single(n, a, b, answer);
+	bool is_correct = true;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			if (!nearly_equal(c[i][j], answer[i][j], 0)) {
+				is_correct = false;
+				break;
+			}
+
+	printf("%s\n", is_correct ? "correct" : "wrong");
+	*/
+
+	//print_array(n, c);
+	//print_array(n, answer);
 
 	return 0;
 }
@@ -104,9 +132,9 @@ void print_array(int n, double arr[][n])
 {
 	int i, j;
 
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < 1; i++) {
 		for (j = 0; j < n; j++) 
-			printf("%lf ", arr[i][j]);
+			printf("%g ", arr[i][j]);
 		printf("\n");
 	}
 }
@@ -167,9 +195,9 @@ void *mul_matrix(void *arg)
 	struct task *t;
 	double (*local_sum)[n], (*a)[n], (*b)[n], (*c)[n];
 	
-	a = aux->a;
-	b = aux->b;
-	c = aux->c;
+	a = (double (*)[n]) aux->a;
+	b = (double (*)[n]) aux->b;
+	c = (double (*)[n]) aux->c;
 
 	if ((local_sum = malloc(sizeof(double) * (n * n))) == NULL) {
 		fprintf(stderr, "malloc error: %d\n", __LINE__);
@@ -179,18 +207,17 @@ void *mul_matrix(void *arg)
 
 	while ((t = get_task()) != NULL) {
 		int size = t->size;
-		int sum;
-		for (i = i_begin = t->i; i < i_begin + size; i++) {
-			for (j = j_begin = t->j; j < j_begin + size; j++) {
-				sum = 0;
+		for (i = 0; i < n; i++) {
+			for (j = j_begin = t->i; j < j_begin + size; j++) {
+				double sum = 0;
 				for (k = k_begin = t->j; k < k_begin + size; k++) {
-					//printf("%d %d %d\n", i, j, k);
 					sum += a[i][k] * b[k][j];
 				}
 				local_sum[i][j] += sum;
 			}
 		}
 	}
+
 
 	pthread_mutex_lock(&c_lock);
 	for (i = 0; i < n; i++)
@@ -199,4 +226,38 @@ void *mul_matrix(void *arg)
 	pthread_mutex_unlock(&c_lock);
 
 	return NULL;
+}
+
+void mul_matrix_single(int n, double (*a)[n], double (*b)[n], double (*answer)[n])
+{
+	int i, j, k;
+
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			for (k = 0; k < n; k++)
+				answer[i][j] += a[i][k] * b[k][j];
+}
+
+bool nearly_equal(double a, double b, double epsilon) 
+{
+	double abs_a = fabs(a);
+	double abs_b = fabs(b);
+	double diff = fabs(a - b);
+	double double_min_normal;
+	long temp = 1;
+
+	double_min_normal = *((double *) &temp);
+
+	if (epsilon == 0)
+		epsilon = 0.00001;
+
+	if (a == b) { // shortcut, handles infinities
+		return true;
+	} else if (a == 0 || b == 0 || diff < double_min_normal) {
+		// a or b is zero or both are extremely close to it
+		// relative error is less meaningful here
+		return diff < (epsilon * double_min_normal);
+	} else { // use relative error
+		return diff / min((abs_a + abs_b), DBL_MAX) < epsilon;
+	}
 }
---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+./dense 1000 1 > t1_1
+./dense 1000 3 > t1_3
+./dense 1000 6 > t1_6
+./dense 3000 1 > t2_1
+./dense 3000 3 > t2_3
+./dense 3000 6 > t2_6
+./dense 5000 1 > t3_1
+./dense 5000 3 > t3_3
+./dense 5000 6 > t3_6
