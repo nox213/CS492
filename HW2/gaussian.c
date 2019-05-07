4c1a14373364e508f0a739beb3c59c54792eff8e
yoonjae <yoonjae@kaist.ac.kr>
2019-05-07 17:57:30 +0900

    Add omp version for gaussian

--- HW2/gaussian.c
+++ HW2/gaussian.c
@@ -4,29 +4,17 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <time.h>
+#include <omp.h>
 
 #define NANO_TO_SEC(x) ((double) ((x) / 10e9))
 #define NANO_TO_MILI(x) ((long) ((x) / 10e6))
 #define SEC_TO_NANO(x) ((long) ((x) * 10e9))
 #define SEC_TO_MILI(x) ((long) ((x) * 10e3))
 
-struct args {
-	double **a, *b;
-	int n;
-	int p;
-	int t_num;
-};
-
-bool is_ready;
-
-pthread_barrier_t barrier_thread;
-
 void print_array(int n, double arr[][n]);
 void swap_row(int n, double arr[][n], double b[n], int x, int y);
 void single_elimination(int n, double a[][n], double b[n]);
-void *elimination(void *arg);
 void back_substitution(int n, double a[][n], double b[n], double x[n]);
 int maxloc(int start, int n, double (*a)[n]);
 bool nearly_equal(double a, double b, double epsilon); 
@@ -46,7 +34,7 @@ int main(int argc, char *argv[])
 	int n, p;
 	int i, j, k;
 	struct timespec begin, end;
-	long elapsed;
+	long elapsed_s, elapsed_p;
 
 	if (argc < 3) {
 		fprintf(stderr, "gaussian.out n p\n");
@@ -57,8 +45,6 @@ int main(int argc, char *argv[])
 	p = strtol(argv[2], NULL, 10);
 
 	double (*a)[n], (*b), (*x), (*serial_a)[n], (*serial_b), (*serial_x), (*A)[n], (*B);
-	pthread_t p_threads[p];
-	struct args aux[p];
 
 	if ((a = malloc(sizeof(double) * (n * n))) == NULL) {
 		fprintf(stderr, "malloc error: %d\n", __LINE__);
@@ -95,7 +81,6 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	memset(p_threads, 0, sizeof(p_threads));
 	memset(a, 0, sizeof(double) * (n * n));
 	memset(b, 0, sizeof(double) * n);
 	memset(x, 0, sizeof(double) * n);
@@ -110,63 +95,48 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	pthread_barrier_init(&barrier_thread, NULL, p);
-
-	printf("Multi thread computaion start\n");
-	clock_gettime(CLOCK_REALTIME, &begin);
-
-	is_ready = false;
-	for (i = 1; i < p; i++) {
-		aux[i].a = (double **) a;
-		aux[i].b =  b;
-		aux[i].n = n;
-		aux[i].p = p;
-		aux[i].t_num = i + 1;
-		pthread_create(&p_threads[i], NULL, elimination, (void *) &aux[i]);
-	}
-
-	int t_num = 1;
-	int start, block_size;
-	for (j = 0; j < n - 1; j++) {
-		int row_max = maxloc(j, n, a);
-		swap_row(n, a, b, j, row_max); 
-		pthread_barrier_wait(&barrier_thread);
-		block_size = (n - 1 - j) / p;
-		start = (j + 1) + (block_size * (t_num - 1));
-		for (i = start; i < min(start + block_size, n); i++) {
-			double m = a[i][j] / a[j][j];
-			for (k = j; k < n; k++) 
-				a[i][k] -= m * a[j][k];
-			b[i] -= m * b[j];
-		}
-		pthread_barrier_wait(&barrier_thread);
-	}
-	back_substitution(n, a, b, x);
-
-	clock_gettime(CLOCK_REALTIME, &end);
-
-	elapsed = SEC_TO_NANO(end.tv_sec - begin.tv_sec) + ((end.tv_nsec - begin.tv_nsec));
-	printf("Multi thread computaion end\n");
-	printf("elapsed time: %lf (sec)\n", NANO_TO_SEC(elapsed));
-
-	double l2norm = 0;
-	for (i = 0; i < n; i++) {
-		double ax = 0;
-		for (j = 0; j < n; j++)
-			ax += A[i][j] * x[j];
-		l2norm += (ax - B[i]) * (ax - B[i]);
-
-	}
-	printf("l2norm: %g\n", sqrt(l2norm));
-
 	printf("Single thread computaion start\n");
 	clock_gettime(CLOCK_REALTIME, &begin);
 	single_elimination(n, serial_a, serial_b);
 	back_substitution(n, serial_a, serial_b, serial_x);
 	clock_gettime(CLOCK_REALTIME, &end);
-	elapsed = SEC_TO_NANO(end.tv_sec - begin.tv_sec) + ((end.tv_nsec - begin.tv_nsec));
+	elapsed_s = SEC_TO_NANO(end.tv_sec - begin.tv_sec) + ((end.tv_nsec - begin.tv_nsec));
 	printf("Single thread computaion end\n");
-	printf("elapsed time: %lf (sec)\n", NANO_TO_SEC(elapsed));
+	printf("elapsed time: %lf (sec)\n", NANO_TO_SEC(elapsed_s));
+
+	double l2norm = 0;
+	for (i = 0; i < n; i++) {
+		double ax = 0;
+		for (j = 0; j < n; j++)
+			ax += A[i][j] * serial_x[j];
+		l2norm += (ax - B[i]) * (ax - B[i]);
+
+	}
+	printf("l2norm of single: %g\n", sqrt(l2norm));
+
+
+	printf("Multi thread computaion start\n");
+	clock_gettime(CLOCK_REALTIME, &begin);
+
+	omp_set_num_threads(p);
+	for (j = 0; j < n - 1; j++) {
+		int row_max = maxloc(j, n, a);
+		swap_row(n, a, b, j, row_max); 
+		#pragma omp parallel for private(i, k) schedule(static)
+		for (i = j + 1; i < n; i++) {
+			double m = a[i][j] / a[j][j];
+			for (k = j; k < n; k++) 
+				a[i][k] -= m * a[j][k];
+			b[i] -= m * b[j];
+		}
+	}
+	back_substitution(n, a, b, x);
+
+	clock_gettime(CLOCK_REALTIME, &end);
+
+	elapsed_p = SEC_TO_NANO(end.tv_sec - begin.tv_sec) + ((end.tv_nsec - begin.tv_nsec));
+	printf("Multi thread computaion end\n");
+	printf("elapsed time: %lf (sec)\n", NANO_TO_SEC(elapsed_p));
 
 	l2norm = 0;
 	for (i = 0; i < n; i++) {
@@ -176,7 +146,9 @@ int main(int argc, char *argv[])
 		l2norm += (ax - B[i]) * (ax - B[i]);
 
 	}
-	printf("l2norm of serial: %g\n", sqrt(l2norm));
+	printf("l2norm of multi: %g\n", sqrt(l2norm));
+
+	printf("Speed up is %g\n", (double) elapsed_s / elapsed_p);
 
 	return 0;
 }
@@ -250,38 +222,6 @@ void swap_row(int n, double arr[][n], double b[n], int x, int y)
 	t = b[x] ;
 	b[x] = b[y];
 	b[y] = t;
-}
-
-void *elimination(void *arg)
-{
-	struct args *aux = (struct args *) arg;
-	int n = aux->n, p = aux->p;
-	int t_num = aux->t_num;
-	int step;
-	int i, j;
-	int block_size, start;
-	double (*a)[n], (*b);
-
-	a = (double (*)[n]) aux->a;
-	b = aux->b;
-
-	for (step = 0; step < n - 1; step++) {
-		pthread_barrier_wait(&barrier_thread);
-		block_size = (n - 1 - step) / p;
-		start = (step + 1) + (block_size * (t_num - 1));
-		/* Remained row is caculated by last thread */
-		if (p == t_num) 
-			block_size += (n - 1 - step) % p;
-		for (i = start; i < min(start + block_size, n); i++) {
-			double m = a[i][step] / a[step][step];
-			for (j = step; j < n; j++) 
-				a[i][j] -= m * a[step][j];
-			b[i] -= m * b[step];
-		}
-		pthread_barrier_wait(&barrier_thread);	
-	}
-
-	return NULL;
 }
 
 void back_substitution(int n, double a[][n], double b[n], double x[n])
