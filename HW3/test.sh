2633708df441f1dced259ea88171142c87c9f9c3
yoonjae <yoonjae@kaist.ac.kr>
2019-06-02 21:46:28 +0900

    Modify test script

--- HW3/test.sh
+++ HW3/test.sh
@@ -1,27 +1,15 @@
 #!/bin/sh
 
-echo 'n = 1000 p = 2'
-./dense.out 1000 2 > t1_1
-echo 'n = 1000 p = 4'
-./dense.out 1000 4 > t1_3
-echo 'n = 1000 p = 6'
-./dense.out 1000 6 > t1_6
-echo 'n = 3000 p = 2'
-./dense.out 3000 2 > t2_1
-echo 'n = 3000 p = 4'
-./dense.out 3000 4 > t2_3
-echo 'n = 3000 p = 6'
-./dense.out 3000 6 > t2_6
-echo 'n = 5000 p = 2'
-./dense.out 5000 2 > t3_1
-echo 'n = 5000 p = 4'
-./dense.out 5000 4 > t3_3
-echo 'n = 5000 p = 6'
-./dense.out 5000 6 > t3_6
+echo 'n = 1000'
+./dense.out 1000> t1
+echo 'n = 3000'
+./dense.out 3000 > t2
+echo 'n = 5000'
+./dense.out 5000 > t3
 
 rm -f result.txt
 touch result.txt
-cat t1_1 t1_3 t1_6 t2_1 t2_3 t2_6 t3_1 t3_3 t3_6 > result.txt
-rm t1_*
-rm t2_*
-rm t3_*
+cat t1 t2 t3 > result.txt
+rm t1
+rm t2
+rm t3
--- HW3/test_gaussian.sh
+++ HW3/test_gaussian.sh
@@ -1,35 +1,15 @@
 #!/bin/sh
 
-echo 'n = 1000 p = 2' > t1_1
-./gaussian.out 1000 2 > t1_1
-echo '----------------------'
-echo 'n = 1000 p = 4'
-./gaussian.out 1000 4 > t1_3
-echo '----------------------'
-echo 'n = 1000 p = 6'
-./gaussian.out 1000 6 > t1_6
-echo '----------------------' 
-echo 'n = 3000 p = 2'
-./gaussian.out 3000 2 > t2_1
-echo '----------------------'
-echo 'n = 3000 p = 4'
-./gaussian.out 3000 4 > t2_3
-echo '----------------------'
-echo 'n = 3000 p = 6'
-./gaussian.out 3000 6 > t2_6
-echo '----------------------'
-echo 'n = 5000 p = 2'
-./gaussian.out 5000 2 > t3_1
-echo '----------------------'
-echo 'n = 5000 p = 4'
-./gaussian.out 5000 4 > t3_3
-echo '----------------------'
-echo 'n = 5000 p = 6'
-./gaussian.out 5000 6 > t3_6
+echo 'n = 1000'
+./gaussian.out 1000 > t1
+echo 'n = 3000'
+./gaussian.out 3000 > t2
+echo 'n = 5000'
+./gaussian.out 5000 > t3
 
 rm -f result_gaussian.txt
 touch result_gaussian.txt
-cat t1_1 t1_3 t1_6 t2_1 t2_3 t2_6 t3_1 t3_3 t3_6 > result_gaussian.txt
-rm t1_*
-rm t2_*
-rm t3_*
+cat t1 t2 t3 > result_gaussian.txt
+rm t1
+rm t2
+rm t3
